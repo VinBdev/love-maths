19dc7278cdf659731d8786dba2fedd0933dc0614
VinBdev <vbpod77@gmail.com>
2021-10-22 17:20:54 +0000

    Added score tracking for correct/ incrorect asnswer

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,8 +50,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! You got it right! :D ");
+        incrementScore();
     } else {
         alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}`);
+        incrementWrongAnswer();
     }
     runGame(calculatedAnswer[1]);
  
@@ -76,6 +78,8 @@ function calculateCorrectAnswer() {
 }
 
 function incrementScore() {
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 
 }
 
